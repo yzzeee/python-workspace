5e6149b7785c45f91f759678af70d640ba55512d
yzzeee <goooooodgood@gmail.com>
2022-03-29 23:35:08 +0900

    변수입력과 연산자

--- main.py
+++ main.py
@@ -43,5 +43,24 @@ print(b, end=' ')
 print(b, end=' ')
 
 '''
-변수입력과
+변수입력과 연산자
 '''
+# a=input("숫자를 입력하시오 : ")
+# print(a)
+a, b=input("숫자를 입력하시오 : ").split()
+print(a+b) # 문자형으로 나옴
+a=int(a)
+print(type(a))
+a, b=map(int, input("숫자를 입력하세요 : ").split())
+print(a+b)
+print(a-b)
+print(a*b)
+print(a/b)
+print(a//b) # 몫
+print(a%b) # 나머지
+print(a**b) # 거듭제곱
+a=4.3
+b=5
+c=a+b
+print(c, type(c))
+
